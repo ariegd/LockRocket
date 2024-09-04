00ab3672a958ff0de3bb358b7451a8855e934329
Zodd <zodd@Zodd-PC.home>
2024-09-05 01:31:05 +0200

    registerUser in controllers/auth.js commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -10,9 +10,19 @@ module.exports = {
       res.render('login');
     },
   
-    registerUser: (req, res) => {
-      // TODO: complete
-      res.redirect('register');
+    registerUser: async (req, res) => {
+      const { name, email, password } = req.body;
+      if(!name || !email || !password) {
+        return res.render('register', { error: 'Please fill all fields' });
+      }
+    
+      if(await User.findOne({where: {email}})) {
+        return res.render('register', { error: 'A user account already exists with this email' });
+      }
+    
+      await User.create({name, email, password: bcrypt.hashSync(password, 8)});
+    
+      res.redirect('login?registrationdone');
     },
   
     loginUser: (req, res) => {
