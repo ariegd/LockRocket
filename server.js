76c297be2c7b34e75cad2a2f2bdbebc4fe38144a
Zodd <zodd@Zodd-PC.home>
2024-09-04 16:00:56 +0200

    database connection before running the server  commit

--- server.js
+++ server.js
@@ -2,14 +2,18 @@ const path = require('path');
 const express = require('express');
 const authRoutes = require('./routes/auth');
 const dashboardRoutes = require('./routes/dashboard');
+const db = require('./db.js');
 
 const app = express();
 const PORT = 8080;
 
+app.use(express.urlencoded({extended: false}));
 app.set("views", path.join(__dirname, "views"));
 app.set('view engine', 'pug');
-
 app.use('/', authRoutes);
 app.use('/', dashboardRoutes);
 
-app.listen(PORT, console.log('Server is running on port: ' + PORT));
+db.sync({ force: false })
+  .then(() => {
+    app.listen(PORT, console.log('Server is running on port: ' + PORT));
+  }); 
