fc1fa0651e26bb93c41199863573d169d66db82e
Zodd <zodd@Zodd-PC.home>
2024-09-04 11:28:33 +0200

    render pug commit

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const PORT = 8080;
 
-app.get('/', (req, res) => res.send('<h1>Hello Express</h1>'));
+app.set("views", path.join(__dirname, "views"));
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => res.render('dashboard'));
+app.get('/login', (req, res) => res.render('login'));
+app.get('/register', (req, res) => res.render('register'));
 
 app.listen(PORT, console.log('Server is running on port: ' + PORT));
