0864022b6ac204e375e2710d6d3f32c53cb54091
Zodd <zodd@Zodd-PC.home>
2024-09-05 11:25:07 +0200

    Express session commit

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const path = require('path');
 const express = require('express');
+const passport = require('passport');
+const session = require('express-session');
 const authRoutes = require('./routes/auth');
 const dashboardRoutes = require('./routes/dashboard');
 const db = require('./db.js');
+const { init: initAuth } = require('./auth');
 
 const app = express();
 const PORT = 8080;
@@ -10,6 +13,17 @@ const PORT = 8080;
 app.use(express.urlencoded({extended: false}));
 app.set("views", path.join(__dirname, "views"));
 app.set('view engine', 'pug');
+
+initAuth();
+app.use(session({
+  secret: 'secret',
+  saveUninitialized: true,
+  resave: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', authRoutes);
 app.use('/', dashboardRoutes);
 
